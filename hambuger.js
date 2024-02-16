c724c326f71b1d20db47557f4fe24382f8330895
Jmorrissette1 <J.morrissette47@gmail.com>
2024-02-15 16:30:27 -0800

    Navigation done

--- hambuger.js
+++ hambuger.js
@@ -1,6 +1,27 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-hamburger.addEventListener('click', () => {
-  nav.classList.toggle('open');
-});
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "flex") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "flex";
+    }
+  });
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
