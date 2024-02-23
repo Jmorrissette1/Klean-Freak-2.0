e734c63a5150b117997b7f920fd5a3509f184896
Jmorrissette1 <J.morrissette47@gmail.com>
2024-02-22 16:35:51 -0800

    Started packages page
    started packages page

---
+++ package-box.js
@@ -0,0 +1,17 @@
+const tabBtns = document.querySelectorAll('.tab-btn');
+const tabs = document.querySelectorAll('.tab');
+
+tabBtns.forEach(btn => {
+  btn.addEventListener('click', openTab);
+});
+
+function openTab(e) {
+  // Toggle active class on buttons
+  tabBtns.forEach(btn => btn.classList.remove('active'));
+  e.currentTarget.classList.add('active');
+  
+  // Open tab content
+  const tabNumber = e.currentTarget.getAttribute('data-tab');
+  tabs.forEach(tab => tab.classList.remove('active'));
+  document.querySelector(`.tab[data-tab="${tabNumber}"]`).classList.add('active');
+}
